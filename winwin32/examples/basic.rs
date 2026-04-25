066abf8ae472c78d5592ca13e4e3ff4b4c836474
SkyGrel19 <skibav2002@gmail.com>
2026-04-25 22:36:47 +0300

    Manual windows event loop implementation

--- winwin32/examples/basic.rs
+++ winwin32/examples/basic.rs
@@ -1,10 +1,17 @@
 use log::LevelFilter;
+use sparkles::config::SparklesConfig;
 use winwin32::run_platform_loop;
 use winwin32::window::Window;
 
 fn main() {
     simple_logger::SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
+    let g = sparkles::init(SparklesConfig::default()
+        .without_file_sender()
+        .with_udp_multicast_default());
 
+    // sparkles::wait_client_connected();
+
+    let g = sparkles::range_event_start!("The whole program");
     let win = Window::new();
     run_platform_loop();
 }
--- winwin32/src/lib.rs
+++ winwin32/src/lib.rs
@@ -1,45 +1,44 @@
-use std::{mem, ptr};
+use std::{mem, ptr, thread};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Duration;
+use log::info;
+use sparkles::range_event_start;
 use windows_sys::Win32::Foundation::{GetLastError, WAIT_FAILED};
 use windows_sys::Win32::System::Threading::INFINITE;
-use windows_sys::Win32::UI::WindowsAndMessaging::{DispatchMessageW, MsgWaitForMultipleObjectsEx, PeekMessageW, TranslateMessage, MSG, MWMO_INPUTAVAILABLE, PM_REMOVE, QS_ALLINPUT};
+use windows_sys::Win32::UI::WindowsAndMessaging::{DispatchMessageW, GetMessageW, MsgWaitForMultipleObjectsEx, PeekMessageW, TranslateMessage, MSG, MWMO_INPUTAVAILABLE, PM_REMOVE, QS_ALLINPUT};
+use crate::window::HANDLED;
 
 pub mod window;
 
+
 pub fn run_platform_loop() {
-    loop {
-        wait_for_message();
-        dispatch_messages();
-    }
-}
-
-fn wait_for_message() {
-    unsafe {
-        let result = MsgWaitForMultipleObjectsEx(
-            0,
-            [ptr::null_mut()].as_ptr() as *const _,
-            INFINITE,
-            QS_ALLINPUT,
-            MWMO_INPUTAVAILABLE,
-        );
-        if result == WAIT_FAILED {
-            log::warn!("Failed to MsgWaitForMultipleObjectsEx: error code {}", GetLastError());
-        }
-    }
-}
-
-fn dispatch_messages() {
     let mut msg: MSG = unsafe { mem::zeroed() };
 
+    thread::spawn(|| {
+        let mut cnt = 0;
+        loop {
+            thread::sleep(Duration::from_secs(1));
+            let cur = HANDLED.load(Ordering::Relaxed);
+            let new = cur - cnt;
+
+            info!("{} e/s", new);
+            cnt = cur;
+        }
+    });
     loop {
         unsafe {
-            if PeekMessageW(&mut msg, ptr::null_mut(), 0, 0, PM_REMOVE) == false.into() {
+            let g = range_event_start!("GetMessage");
+            if GetMessageW(&mut msg, ptr::null_mut(), 0, 0) == false.into() {
+                info!("Exiting from platform loop...");
                 break;
             }
+            drop(g);
 
 
             TranslateMessage(&msg);
+            let g = range_event_start!("DispatchMessage");
             DispatchMessageW(&msg);
+            drop(g);
         }
     }
-
 }
